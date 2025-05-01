dc006a53d6a19623a7211a21eaf6242e59db878a
ZonBock1337 <zonigaming1337@gmail.com>
2025-05-01 22:13:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const express = require('express');
-import fetch from 'node-fetch';
+// Verwende 'import' anstelle von 'require'
+import express from 'express';
+import fetch from 'node-fetch'; // Hier auch 'import' verwenden
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
